14ed97bbd4d1833bdcde969b9fb03f0b4fbff6c7
coryn <coloncoryn@gmail.com>
2023-03-15 13:37:05 -0400

    write loop to iterate through all winning scenarios

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 /*----- constants -----*/
 
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 
 /*----- app's state (variables) -----*/
 
@@ -59,14 +69,28 @@ function handleTurn(event) {
 
             turn = turn === 'X' ? 'O' : 'X'; 
 
-            // short hand for if statement 
-            win = board[0] && board[0] === board[1] && board[0] === board[2] ? board[0] : null;
+            win = getWinner();  
 
-            
             render();
+
+
             console.log(win);
 
             };
 
            
-            
+function getWinner() {
+                   
+                    let winner = null;
+
+                    winningCombos.forEach(function(combo, index) {
+
+                        if (board[combo[0]] &&
+                            board[combo[0]] === board[combo[1]] &&
+                            board[combo[0]] === board[combo[2]]) {
+                                winner = board[combo[0]];
+                            }
+                    });
+                    
+                    return winner;
+                };
