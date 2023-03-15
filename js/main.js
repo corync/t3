475a4f6051de8f6c5bc06dae50a0ad14ea708cdb
coryn <coloncoryn@gmail.com>
2023-03-15 13:03:47 -0400

    js creating X turn, addEventListener on click

--- js/main.js
+++ js/main.js
@@ -2,12 +2,16 @@
 /*----- app's state (variables) -----*/
 
 let board;
+let turn = 'X'
 
 /*----- cached element references -----*/
 
 const squares = Array.from(document.querySelectorAll('#board div'));
 
 /*----- event listeners -----*/
+
+document.getElementById('board').addEventListener('click', handleTurn);
+
 /*----- functions -----*/
 
 function init() {
@@ -26,5 +30,21 @@ init();
 function render() {
         board.forEach(function(mark, index){
         console.log(mark, index);
+
+        //this sets the text content of the square of the same position to the mark on the board.
+        squares[index].textContent = mark;
         });
         };
+
+function handleTurn(event) {
+            let idx = squares.findIndex(function(square) {
+            return square === event.target;
+            });
+
+            board[idx] = turn;
+            // check your console logs to make sure it's working!
+            console.log(board);
+
+
+            };
+
